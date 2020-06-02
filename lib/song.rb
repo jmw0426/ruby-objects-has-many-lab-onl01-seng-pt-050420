a94fd699d1ba55a48f322255ddd27b6ad8f4e7d7
Joshua Williams <jmw0426@hotmail.com>
2020-06-02 17:39:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,23 @@
-class Songs 
+class Song 
   attr_accessor :title, :artist
   
   @@all = []
-  
+
   def initialize(title)
     @title = title
+    save
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
