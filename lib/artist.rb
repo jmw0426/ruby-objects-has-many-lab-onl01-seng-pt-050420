2f8eb728961cf96313d3d891c17bd5b259794df9
Joshua Williams <jmw0426@hotmail.com>
2020-06-02 17:39:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,20 @@
 class Artist
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
-    @songs = []
   end
-  
-  def songs
-    @songs
+ 
+  def add_song(song)
+    song.artist = self
   end
 
+  def add_song_by_name(title)
+    song = Song.new(title)
+    add_song(song)
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 end
